308324d2bdf59a20c39d93e36a36f0a4a6218be0
FabriEstrada <faxzzpng@gmail.com>
2026-05-04 09:24:20 -0500

    Agregué funcion Mostrar_Ventas

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ def Registro_Ventas(nombre,cantidad,precio):
         total_pago = cantidad *precio
         archivo.write(f"{nombre},{cantidad},{precio},{total_pago}\n")
         print(f"Se introdujo el producto: {nombre}\n")
+
+def Mostrar_Ventas():
+    try:
+        with open('REGISTRO-VENTAS/ventas.txt','r',encoding='utf-8') as archivo:
+            contenido = archivo.readlines()
+            for linea in contenido:
+                if contenido:
+                    print(f"{linea.strip()}\n")
+                
+                else:
+                    print("No se ha registrado elementos")
+    
+    except FileNotFoundError:
+        print("El archivo mencionado no se ha encontrado")

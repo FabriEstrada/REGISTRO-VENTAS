a8b859c391677d3b45e04aaf5a0fcb39b9641bb8
FabriEstrada <faxzzpng@gmail.com>
2026-05-04 09:50:07 -0500

    Agregué funcion Ingreso_Datos

--- main.py
+++ main.py
@@ -17,3 +17,17 @@ def Mostrar_Ventas():
     
     except FileNotFoundError:
         print("El archivo mencionado no se ha encontrado")
+
+def Ingreso_Datos():
+    nombre_producto = input("Introduce el nombre del producto:\t")
+    
+    while True:
+        try:
+            cantidad_producto = int(input("Introduce la cantidad del producto:\t"))
+            precio_producto = float(input("Introduce el precio del producto:\t"))
+            break
+        
+        except ValueError:
+            print("Error al ingresar los datos, vuelve a intentarlo")
+    
+    return nombre_producto, cantidad_producto, precio_producto

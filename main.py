c473739af37333f4d873dc88d7f9aa38c1af4adf
FabriEstrada <faxzzpng@gmail.com>
2026-05-04 11:36:32 -0500

    Solucioné los errores del programa

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def Registro_Ventas(nombre,cantidad,precio):
-    with open('REGISTRO-VENTAS/ventas.txt','a',encoding='utf-8') as archivo:
+    with open('ventas.txt','a',encoding='utf-8') as archivo:
         total_pago = cantidad *precio
         archivo.write(f"{nombre},{cantidad},{precio},{total_pago}\n")
         print(f"Se introdujo el producto: {nombre}\n")
 
 def Mostrar_Ventas():
     try:
-        with open('REGISTRO-VENTAS/ventas.txt','r',encoding='utf-8') as archivo:
+        with open('ventas.txt','r',encoding='utf-8') as archivo:
             contenido = archivo.readlines()
             for linea in contenido:
                 if contenido:
